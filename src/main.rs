bc750c878c64cf7c744cc19224d6801eb2791fdc
MatteoL <matteo.lingiardi@eudata.com>
2021-11-07 16:10:39 +0100

    added git2

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,9 @@ use std::env;
 
 fn main() -> std::io::Result<()>{
     //Step 1 get the current path and print it to screen
-    let path = env::current_dir()?;
-    println!("This is the current directory {:?}", path);
+    let path_buf = env::current_dir()?;
+    let path = path_buf.as_path(); 
+    println!("This is the current directory {:?}", &path);
+    //Step 2 get show branches of the current directory
     Ok(())
 }
