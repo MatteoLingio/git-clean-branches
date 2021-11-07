d5b487d12c65f1ed31330cef28ff902efd9dfb55
MatteoL <matteo.lingiardi@eudata.com>
2021-11-07 15:58:44 +0100

    got current directory

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
-fn main() {
-    println!("Hello, world!");
+use std::env;
+
+fn main() -> std::io::Result<()>{
+    //Step 1 get the current path and print it to screen
+    let path = env::current_dir()?;
+    println!("This is the current directory {:?}", path);
+    Ok(())
 }
