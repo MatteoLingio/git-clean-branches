bd058be458eda291d8f47740a2947d5dc441f00d
MatteoL <matteo.lingiardi@eudata.com>
2021-11-08 15:47:13 +0100

    completed get_branches

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,11 @@
 use std::{env};
-use git2::{BranchType, Repository};
+use git2::{BranchType, Oid, Repository};
 use chrono::{NaiveDateTime, Duration};
 fn main() -> std::io::Result<()>{
-    //Step 1 get the current path and print it to screen
     let path_buf = env::current_dir()?;
     let path = path_buf.as_path(); 
     println!("This is the current directory {:?}", &path);
-    //Step 2 get show branches of the current directory
     //Try to open the repository
-
     let repo = match Repository::open(path)  {
        Ok(repo) => repo,
        Err(e) => panic!("Failed to open the repo in directory {} for error {}", &path.display(), e)
@@ -17,25 +14,29 @@ fn main() -> std::io::Result<()>{
     let branches = get_branches(&repo).unwrap();
 
     for b in branches {
-        println!("branch {:?} - last commit time {}", b.name, b.time);
+        println!("branch {:?} - last commit {} - at {}", b.name, b.last_commit_id, b.time);
     }
     Ok(())
 }
 
+//creates a Vector of Branch with the corerct informations with main/master filtered out
+//The vector is sorted by oldest commit
 fn get_branches(repo: &Repository) -> Result<Vec<Branch>, git2::Error> {
+
     let mut branches = repo
         .branches(Some(BranchType::Local))?
         .map(|branch| {
             let (branch, _) = branch?;
             let name = String::from_utf8(branch.name_bytes()?.to_vec()).unwrap();
             let commit = branch.get().peel_to_commit()?;
+            let last_commit_id = commit.id();
             let offset = Duration::minutes(i64::from(commit.time().offset_minutes()));
             let time = NaiveDateTime::from_timestamp(commit.time().seconds(), 0) + offset;
-            // println!("the time of commit is {:?}", time);
             Ok(Branch {
                 name,
                 branch,
-                time
+                time,
+                last_commit_id
             })
         })
         .filter(|branch| {
@@ -49,12 +50,13 @@ fn get_branches(repo: &Repository) -> Result<Vec<Branch>, git2::Error> {
         .collect::<Result<Vec<_>, git2::Error>>()?;
 
     branches.sort_unstable_by_key(|branch| branch.time);
-    
+
     Ok(branches)
 }
 
 struct Branch<'repo> {
     name: String,
     branch: git2::Branch<'repo>,
-    time: NaiveDateTime
+    time: NaiveDateTime,
+    last_commit_id: Oid
 }
