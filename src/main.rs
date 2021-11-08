d739d686c1d4050c57afe3007583185c8cf67b98
MatteoL <matteo.lingiardi@eudata.com>
2021-11-08 15:08:57 +0100

    completed get_branches

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
-use std::env;
+use std::{env};
 use git2::{BranchType, Repository};
+use chrono::{NaiveDateTime, Duration};
 fn main() -> std::io::Result<()>{
     //Step 1 get the current path and print it to screen
     let path_buf = env::current_dir()?;
@@ -13,26 +14,47 @@ fn main() -> std::io::Result<()>{
        Err(e) => panic!("Failed to open the repo in directory {} for error {}", &path.display(), e)
     };
 
-
-    let branches = match repo.branches(Some(BranchType::Local)){
-        Ok(branches) => branches,
-        Err(e) => panic!("Failed to get branches in this repository: {}", e)
-    };
+    let branches = get_branches(&repo).unwrap();
 
     for b in branches {
-        let (branch, _) = b.unwrap();
-        let commit = branch.get().peel_to_commit().unwrap();
-        let sum = commit.summary().unwrap();
-        match branch.name() {
-            Ok(name) => {
-                println!("branch: {} {}",name.unwrap(), &sum);
-                if name != Some("develop") && name != Some("master") && name != Some("main"){
-                    println!("{}", name.unwrap());
-                }
-            },
-            Err(_) => {},
-        };
+        println!("branch {:?} - last commit time {}", b.name, b.time);
     }
-
     Ok(())
+}
+
+fn get_branches(repo: &Repository) -> Result<Vec<Branch>, git2::Error> {
+    let mut branches = repo
+        .branches(Some(BranchType::Local))?
+        .map(|branch| {
+            let (branch, _) = branch?;
+            let name = String::from_utf8(branch.name_bytes()?.to_vec()).unwrap();
+            let commit = branch.get().peel_to_commit()?;
+            let offset = Duration::minutes(i64::from(commit.time().offset_minutes()));
+            let time = NaiveDateTime::from_timestamp(commit.time().seconds(), 0) + offset;
+            // println!("the time of commit is {:?}", time);
+            Ok(Branch {
+                name,
+                branch,
+                time
+            })
+        })
+        .filter(|branch| {
+            return match branch {
+                Ok(branch) => {
+                    branch.name != "master" && branch.name != "main"
+                },
+                Err(e) => panic!("Error {}", e)
+            };
+        })
+        .collect::<Result<Vec<_>, git2::Error>>()?;
+
+    branches.sort_unstable_by_key(|branch| branch.time);
+    
+    Ok(branches)
+}
+
+struct Branch<'repo> {
+    name: String,
+    branch: git2::Branch<'repo>,
+    time: NaiveDateTime
 }
